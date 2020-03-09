cf97f847449ab991194f7e2f899305b989f7368d
Kathryn Clark <kathrynlael@gmail.com>
2020-03-09 16:41:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,11 +13,11 @@ def assembled_aoh
 end
 
 def literal_aoh
-  employees=[[:DON_G => { name:  "Don Gately", occupation: "Live-in Staff" }], 
-  [:JOELLE_VD => { name:  "Joelle van Dyne", occupation: "Radio Personality" }], 
-  [:PAT_M =>  { name:  "Pat Monteseian", occupation: "Staff" }], 
-  [:KATE_G => { name:  "Kate Gompert", occupation: "None" }], 
-  [:BRUCE_G => { name:  "Bruce Green", occupation: "Fan of Mildred" }]]
+  employees=[[{ name:  "Don Gately", occupation: "Live-in Staff" }], 
+  [{ name:  "Joelle van Dyne", occupation: "Radio Personality" }], 
+  [{ name:  "Pat Monteseian", occupation: "Staff" }], 
+  [{ name:  "Kate Gompert", occupation: "None" }], 
+  [{ name:  "Bruce Green", occupation: "Fan of Mildred" }]]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
